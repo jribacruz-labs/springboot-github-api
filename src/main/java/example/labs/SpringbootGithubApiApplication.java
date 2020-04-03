da4b7d9594582c45635a48ea4de5137a52e7b96e
jcruz <jcruz@tre-pa.jus.br>
2020-04-03 08:39:23 -0300

    Ajuste no código de clone

--- src/main/java/example/labs/SpringbootGithubApiApplication.java
+++ src/main/java/example/labs/SpringbootGithubApiApplication.java
@@ -1,18 +1,27 @@
 package example.labs;
 
-import org.kohsuke.github.GHOrganization;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FilenameUtils;
+import org.eclipse.egit.github.core.service.RepositoryService;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
+import org.kohsuke.github.*;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 public class SpringbootGithubApiApplication implements CommandLineRunner {
 
     @Value("${GH_TOKEN}")
     private String ghToken;
+
+    @Value("${XTOOL_HOME}")
+    private Path xtoolHome;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootGithubApiApplication.class, args);
@@ -21,10 +30,30 @@ public class SpringbootGithubApiApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         GitHub github = new GitHubBuilder().withOAuthToken(ghToken).build();
-        GHOrganization organization = github.getOrganization("tre-pa");
-        organization.listRepositories().forEach(o -> System.out.println(o.getName().concat(" ").concat(o.getSshUrl())));
+        this.listOrganizationRepositories(github);
+//        this.cloneRepo("https://github.com/tre-pa/springboot-angular2-keycloak-labs.git");
+
 
     }
 
+    @SneakyThrows
+    private void listOrganizationRepositories(GitHub github) {
+        GHOrganization organization = github.getOrganization("tre-pa");
+        PagedIterable<GHRepository> ghRepositories = organization.listRepositories();
+        for (GHRepository repo : ghRepositories) {
+            System.out.println(String.format("Repo: %s, ssh: %s", repo.getName(), repo.getHttpTransportUrl()));
+        }
+        RepositoryService service = new RepositoryService();
+//        service.getOrgRepositories();
+    }
+
+    @SneakyThrows
+    private void cloneRepo(String sshRepoUrl) {
+//        System.out.println(FilenameUtils.getBaseName(sshRepoUrl));
+        Git git = Git.cloneRepository()
+                .setURI(sshRepoUrl)
+                .setDirectory(xtoolHome.resolve("src").resolve(FilenameUtils.getBaseName(sshRepoUrl)).toFile())
+                .call();
+    }
 
 }
---
+++ src/main/java/example/labs/SshTransportConfigCallback.java
@@ -0,0 +1,24 @@
+package example.labs;
+
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
+import org.eclipse.jgit.api.TransportConfigCallback;
+import org.eclipse.jgit.transport.*;
+import org.eclipse.jgit.util.FS;
+
+public class SshTransportConfigCallback implements TransportConfigCallback {
+
+    private final SshSessionFactory sshSessionFactory = new JschConfigSessionFactory() {
+        @Override
+        protected void configure(OpenSshConfig.Host hc, Session session) {
+//            session.setConfig("StrictHostKeyChecking", "no");
+        }
+    };
+
+    @Override
+    public void configure(Transport transport) {
+        SshTransport sshTransport = (SshTransport) transport;
+        sshTransport.setSshSessionFactory(sshSessionFactory);
+    }
+}
